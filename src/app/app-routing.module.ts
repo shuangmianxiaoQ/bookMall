413d94a5d89fce43413c7fa4e42aba76e928b309
shuangmianxiaoQ <1245814811@qq.com>
2018-04-28 23:36:08 +0800

    图书商城：初步搭建（添加路由模块，添加登录、注册、首页等组件）

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {HeaderComponent} from './header/header.component';
+import {IndexComponent} from './index/index.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+
+const routes : Routes = [
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'register',
+    component: RegisterComponent
+  }, {
+    path: 'header',
+    component: HeaderComponent
+  }, {
+    path: 'index',
+    component: IndexComponent
+  }, {
+    path: '**',
+    component: NotFoundComponent
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    IndexComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
